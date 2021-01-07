95499fddc536d55fb1c97a24061923ed3fa880f0
Ethan Calvert <ethan.calvert@gmail.com>
2021-01-07 11:15:06 -0600

    updated build process

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const fs = require("fs");
 const path = require("path");
 const parseDoc = require("./html-to-json");
-const exec = require("child_process").exec;
+const eleventy = require("@11ty/eleventy");
+
+async function build() {
+  let bld = new eleventy();
+  await bld.init();
+  await bld.write();
+}
 
 let BookMarksSource = "./from-safari/Safari Bookmarks.html";
 if (process.env.mode && process.env.mode == "TEST") {
@@ -10,34 +16,12 @@ if (process.env.mode && process.env.mode == "TEST") {
 
 let bmString = JSON.stringify(parseDoc(BookMarksSource));
 
-// let jsSource = path.resolve(__dirname, "src/_includes/index.js");
-// let jsString = fs.readFileSync(jsSource, "utf8");
-
-// jsString = `const bookMarks = ${bmString}; ${jsString}`;
-
-// let htmlIndexSource = path.resolve(__dirname, "src/index.html");
-// let htmlIndexString = fs.readFileSync(htmlIndexSource, "utf8");
-
-// htmlIndexString = htmlIndexString.replace(
-//   "</html>",
-//   `<script>${jsString}</script></html>`
-// );
-// fs.writeFileSync(
-//   path.resolve(__dirname, "public/index.html"),
-//   htmlIndexString,
-//   "utf8"
-// );
-
 fs.writeFileSync(
-  path.resolve(__dirname, "src/_data/bookmarks.js"),
+  path.resolve(__dirname, "src/_data/bookmarks.json"),
   bmString,
   "utf8"
 );
-exec("npm run build", (err, stdout, stderr) => {
-  if (err) {
-    console.log(err);
-  }
-  console.log(stdout);
-});
 
-fs.unlinkSync(path.resolve(__dirname, BookMarksSource));
+build().then(() => {
+  fs.unlinkSync(path.resolve(__dirname, BookMarksSource));
+});
